0d7f68368b310124dfc02e5e76c9a77394be58a5
Harini Janardhan <94678338+Harini-J306@users.noreply.github.com>
2022-03-04 09:56:19 +0000

    done

--- p2final.c
+++ p2final.c
@@ -10,7 +10,10 @@ fraction input_fraction()
 }
 fraction largest_fraction(fraction f1,fraction f2,fraction f3)
 {
-  
+  fraction f;
+  printf("enter the fraction\n");
+  scanf("%d%d",%f.num,%f.den);
+  return f;
 }
 void output(fraction f1,fraction f2,fraction f3,fraction largest)
 {
