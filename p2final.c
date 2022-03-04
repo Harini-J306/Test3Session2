9ae2e30ef2e98ae3962b14eb6acd69417b052d4b
Harini Janardhan <94678338+Harini-J306@users.noreply.github.com>
2022-03-04 09:56:49 +0000

    .

--- p2final.c
+++ p2final.c
@@ -6,14 +6,14 @@ int num,den;
 typedef_fraction Fraction
 fraction input_fraction()
 {
-  
-}
-fraction largest_fraction(fraction f1,fraction f2,fraction f3)
-{
-  fraction f;
+   fraction f;
   printf("enter the fraction\n");
   scanf("%d%d",%f.num,%f.den);
   return f;
+}
+fraction largest_fraction(fraction f1,fraction f2,fraction f3)
+{
+ 
 }
 void output(fraction f1,fraction f2,fraction f3,fraction largest)
 {
