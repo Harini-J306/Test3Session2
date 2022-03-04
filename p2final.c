870f1a003800f629fd727809b217d7c004a6dfd0
Harini Janardhan <94678338+Harini-J306@users.noreply.github.com>
2022-03-04 09:49:14 +0000

    done

--- p2final.c
+++ p2final.c
@@ -1,1 +1,22 @@
 #include<stdio.h>
+struct_fraction
+{
+int num,den;
+};
+typedef_fraction Fraction
+fraction input_fraction()
+{
+  
+}
+fraction largest_fraction(fraction f1,fraction f2,fraction f3)
+{
+  
+}
+void output(fraction f1,fraction f2,fraction f3,fraction largest)
+{
+  
+}
+int main()
+{
+  
+}
